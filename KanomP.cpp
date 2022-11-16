8c26e7c62e788ecff8f048ceaacb8f28f420d807
primer03 <Panupong.person@gmail.com>
2022-11-16 14:37:42 +0700

    16

--- KanomP.cpp
+++ KanomP.cpp
@@ -21,10 +21,12 @@ string Punit[100] = {};
 string Tid[100] = {};
 string Tname[100] = {};
 string Tprice[100] = {};
+string Ttype[100] = {};
 int Tunit[100] = {};
 string btop[100][3] = {};
 int corder[3] = {};
 int dbpung = 0,bd = 0,cn = 0;
+
 
 struct Product
 {
@@ -56,9 +58,6 @@ void Productlist()
     ifstream read;
     read.open("C:/Pungping/product.txt");
     int x = 0;
-    // cout << "+==========================================+" << endl;
-    // cout << ": ID :         Name         : Price : Unit :" << endl;
-    // cout << "+------------------------------------------+" << endl;
     while (!read.eof())
     {
         string strid;
@@ -77,18 +76,14 @@ void Productlist()
         Tname[x] = pd.name;
         Tprice[x] = pd.priceS;
         Tunit[x] = iunit;
-        // cout << right << ":" << setw(3) << Tid[x] << " : " << setw(20) << Tname[x] << " : " << setw(5) << Tprice[x] << " :" << setw(5) << Tunit[x] << " :" << endl;
         x++;
-        // cout << pd.name << endl;
     }
-    // cout << "+=========================================+" << endl;
     read.close();
 }
 
 void Selecttopping()
 {
     system("CLS");
-    Productlist();
     int topids,chtop,tno,chid = 0,chkn = 0,chre;
     string Sizes, topid;
     char maxnum[50];
@@ -252,6 +247,12 @@ void orderpung(){
             
             }
             cout << "#<=================================>#" << endl;
+            if(Ttype[e] == "n"){
+                cout << "NotToast" << endl;
+            }else{
+                 cout << "Toast" << endl;
+            }
+            cout << "#<=================================>#" << endl;
         }
 }
 void ordercancel(){
@@ -266,98 +267,68 @@ void ordercancel(){
         }
     }
 }
-void pungping(){
-    Selecttopping();
-    int poption;
-    bool ckpung = false;
-    do
-    {
-        cout << "1. NextBread" << endl;
-        cout << "2. ViewOrder" << endl;
-        cout << "3. CancelOrder" << endl;
-        cout << "4. ConfirmOrder" << endl;
-        cout << "Enter : ";
-        cin >> poption;
-        if(poption == 1){
-            Selecttopping();
-            ckpung = false;
-        }else if(poption == 2){
-            orderpung();
-            ckpung = false;
-        }else if(poption == 3){
-            ordercancel();
-            system("CLS");
-            ckpung = true;
-        }
-    } while (ckpung == false);
-}
-
-// void Topping()
-// {
-//     cout << endl;
-//     int conselect;
-//     char can, con;
-//     Selecttopping();
-//     bool ckc = false;
-//     do{
-//         if(Cnum == 2){
-//             cout << endl;
-//             cout << "1.ViewOrder" << endl;
-//             cout << "2.CancelOrder" << endl;
-//             cout << "3.ConfirmOrder" << endl;
-//             cout << "Enter Select : ";
-//             cin >> conselect;
-//             if(conselect == 1){
-//             }else if(conselect == 2){
-//             }else if(conselect == 3){
-//             }else{
-//                 ckc = false;
-//             }
-//         }else{
-//             cout << endl;
-//             cout << "1.Next Topping" << endl;
-//             cout << "2.Confirm" << endl;
-//             cout << "Enter Select : ";
-//             cin >> conselect;
-//             if(conselect == 1){
-//                 Cnum += 1;
-//                 Topping();
-//             }else if(conselect == 2){
-//                 Bread();
-//             }
-//         }
-//     } while (ckc == false);
-// }
-
 
 void Orderbread()
 {
+    Productlist();
     bool check = false;
     do
     {
         cout << "NotToast OR Toast (N/T)" << endl;
-        cout << "Exit (X)" << endl;
+        if(bd == 0){
+            cout << "Exit (X)" << endl;
+        }else{
+            cout << "1. ViewOrder" << endl;
+            cout << "2. CancelOrder" << endl;
+            cout << "3. ConfirmOrder" << endl;
+        }
         char option;
-        string ad_pass;
+        string ad_pass,stroption;
         cout << "Enter option : ";
         cin >> option;
-        if (option == 'N' || option == 'n')
-        {
-            pungping();
-            check = true;
-        }
-        else if (option == 'T' || option == 't')
-        {
-            pungping();
-            check = true;
-        }
-        else if (option == 'X' || option == 'x')
-        {
-            check = true;
-        }
-        else
-        {
-            check = false;
+        stroption = option;
+        if(bd == 0){
+            if (option == 'N' || option == 'n'){
+                transform(stroption.begin(), stroption.end(), stroption.begin(), ::tolower);
+                Ttype[bd] = option;
+                Selecttopping();
+                system("CLS");
+                check = false;
+            }else if (option == 'T' || option == 't'){
+                Ttype[bd] = option;
+                Selecttopping();
+                system("CLS");
+                check = false;
+            }else if (option == 'X' || option == 'x'){
+                check = true;
+            }else{
+                check = false;
+            }
+        }else{
+             if (option == 'N' || option == 'n'){
+                transform(stroption.begin(), stroption.end(), stroption.begin(), ::tolower);
+                Ttype[bd] = option;
+                Selecttopping();
+                system("CLS");
+                check = false;
+            }else if (option == 'T' || option == 't'){
+                Ttype[bd] = option;
+                Selecttopping();
+                system("CLS");
+                check = false;
+            }else if(option == '1'){
+                system("CLS");
+                orderpung();
+                check = false;
+            }else if(option == '2'){
+                ordercancel();
+                system("CLS");
+                check = false;
+            }else if(option == '3'){
+                check = true;
+            }else{
+                check = false;
+            }
         }
     } while (check == false);
 }
@@ -416,9 +387,7 @@ void Readdata()
     }
         cout << "#<============================================>#" << endl;
     read.close();
-
 }
-
 int searchData()
 {
     int id;
@@ -641,59 +610,35 @@ void Creatfile()
     oidfile << "0";
     oidfile.close();*/
     ofstream productfile("C:/Pungping/product.txt");
-    productfile << "\n"
-                << "1";
-    productfile << "\n"
-                << "Sugar";
-    productfile << "\n"
-                << "5";
-    productfile << "\n"
-                << "100";
+    productfile << "\n" << "1";
+    productfile << "\n" << "Sugar";
+    productfile << "\n" << "5";
+    productfile << "\n" << "100";
 
-    productfile << "\n"
-                << "2";
-    productfile << "\n"
-                << "Chocolate";
-    productfile << "\n"
-                << "5";
-    productfile << "\n"
-                << "100";
+    productfile << "\n" << "2";
+    productfile << "\n" << "Chocolate";
+    productfile << "\n" << "5";
+    productfile << "\n" << "100";
 
-    productfile << "\n"
-                << "3";
-    productfile << "\n"
-                << "";
-    productfile << "\n"
-                << "5";
-    productfile << "\n"
-                << "100";
+    productfile << "\n" << "3";
+    productfile << "\n" << "";
+    productfile << "\n" << "5";
+    productfile << "\n" << "100";
 
-    productfile << "\n"
-                << "4";
-    productfile << "\n"
-                << "";
-    productfile << "\n"
-                << "5";
-    productfile << "\n"
-                << "100";
+    productfile << "\n" << "4";
+    productfile << "\n" << "";
+    productfile << "\n" << "5";
+    productfile << "\n" << "100";
 
-    productfile << "\n"
-                << "5";
-    productfile << "\n"
-                << "";
-    productfile << "\n"
-                << "5";
-    productfile << "\n"
-                << "100";
+    productfile << "\n" << "5";
+    productfile << "\n" << "";
+    productfile << "\n" << "5";
+    productfile << "\n" << "100";
 
-    productfile << "\n"
-                << "6";
-    productfile << "\n"
-                << "";
-    productfile << "\n"
-                << "5";
-    productfile << "\n"
-                << "100";
+    productfile << "\n" << "6";
+    productfile << "\n" << "";
+    productfile << "\n" << "5";
+    productfile << "\n" << "100";
     productfile.close();
     /*ofstream orderfile("C:/Pungping/order.txt");
     orderfile.close();*/
